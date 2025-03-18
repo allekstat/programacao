ed10d173ec96a00e7555fbab535a28137a0c9ba3
Alexsander <131073044+allekstat@users.noreply.github.com>
2025-03-18 20:07:00 -0300

    Add files via upload

---
+++ c/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿class Calculadora
+{
+    static void Main()
+    {
+        float numero1, numero2, resultado = 0;
+        char operacao;
+
+        System.Console.WriteLine("Digite o Primeiro Número.");
+        System.Console.Write("-> ");
+        numero1 = float.Parse(System.Console.ReadLine());
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Digite o Segundo Número.");
+        System.Console.Write("-> ");
+        numero2 = float.Parse(System.Console.ReadLine());
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Digite a Operação, Por Exemplo ( +  -  *  / ).");
+        System.Console.Write("-> ");
+        operacao = System.Console.ReadKey().KeyChar;
+        System.Console.WriteLine();
+        System.Console.WriteLine();
+
+        switch (operacao)
+        {
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                if (numero2 == 0)
+                {
+                    operacao = 'q';
+                    System.Console.WriteLine("Não Pode Dividir Por Zero.");
+                }
+                resultado = numero1 / numero2;
+                break;
+            default:
+                operacao = 'q';
+                System.Console.WriteLine("Operação Inválida.");
+                break;
+        }
+
+        if (operacao != 'q')
+        {
+            System.Console.WriteLine("O Resultado é: " + resultado);
+        }
+
+        System.Console.ReadKey(true);
+    }
+}
---
+++ c/Media.cs
@@ -0,0 +1,29 @@
+﻿class Media
+{
+    static void Main()
+    {
+        int[] notas = new int[3];
+        float media;
+
+        System.Console.WriteLine("Digite a Primeira Nota.");
+        System.Console.Write("-> ");
+        notas[0] = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Digite a Segunda Nota.");
+        System.Console.Write("-> ");
+        notas[1] = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("\nDigite a Terceira Nota.");
+        System.Console.Write("-> ");
+        notas[2] = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine();
+
+        media = (float)(notas[0] + notas[1] + notas[2]) / 3;
+
+        System.Console.WriteLine("A Média de Suas Notas é: " + media);
+
+        System.Console.ReadKey(true);
+    }
+}
